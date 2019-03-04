1d2ef3d57acb8e6477fd51f9fee9e2dc29c6a425
Clif Wolfe <clifwolfe1@gmail.com>
2019-03-04 18:25:04 -0500

    automate copying of dependencies

--- mkjail.sh
+++ mkjail.sh
@@ -1,22 +1,32 @@
-mkdir -p /var/jail/{dev,etc,lib,lib64,usr,bin}
-mkdir -p /var/jail/usr/bin
+#!/usr/bin/env sh
+
+if [ $# -eq 0 ]
+then
+	echo "Usage: $0 <username>"
+	exit 1
+fi
+
+
+mkdir -p /var/jail/$1/{dev,etc,lib,lib64,usr,bin}
+mkdir -p /var/jail/$1/usr/bin
 chown root.root /var/jail
-mknod -m 666 /var/jail/dev/null c 1 3
-cp /etc/ld.so.cache /var/jail/etc/
-cp /etc/ld.so.conf /var/jail/etc/
-cp /etc/nsswitch.conf /var/jail/etc/
-cp /etc/hosts/ /var/jail/etc/
-cp /usr/bin/ls /var/jail/usr/bin/
-cp /usr/bin/bash /var/jail/usr/bin/bash
+mknod -m 666 /var/jail/$1/dev/null c 1 3
+cp /etc/ld.so.cache /var/jail/$1/etc/
+cp /etc/ld.so.conf /var/jail/$1/etc/
+cp /etc/nsswitch.conf /var/jail/$1/etc/
+cp /etc/hosts /var/jail/$1/etc/
+cp /usr/bin/{ls,bash,cat} /var/jail/$1/usr/bin/ 
 
 # copy the deps for your binaries 
-#binpath=$(ldd /bin/ls | grep -Po ".+(?= =>)" | awk '{print $1}' | xargs whereis | awk '{print $2}')
+binpath=$(ldd /bin/{ls,bash,cat} | grep -Po ".+(?= =>)" | awk '{print $1}' | xargs whereis | awk '{print $2}')
+for i in $binpath
+do
+	cp --parents $i $(echo "/var/jail/$1")
+done
+
 
 #for sshusers:
 # in /etc/ssh/sshd_config : 
-#Match group sshusers
-#	Chroot Directory /var/jail
-#	X11Forwarding no
-#	AllowTcpForwarding no
+echo "Match group sshusers\n\tChroot Directory /var/jail\n\tX11Forwarding no\n\tAllowTcpForwarding no" >> /etc/ssh/sshd_config
 
 
