8ea840e7d7a96f23ae2415ee8382d096c5cc9479
Clif Wolfe <clifwolfe1@gmail.com>
2019-03-08 16:03:12 -0500

    added addkey.sh

---
+++ addkey.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+if [ "$#" -ne 4]
+then
+	echo "Usage: $0 <hostname/ip> <username> <password> </path/to/pub/key>"
+	exit 1
+fi
+
+# ssh-keygen -t rsa -b 4096 -f <file> -N ""
+pubkey=$(cat $4)
+sshpass -p "$3" ssh -oStrictHostKeyChecking=no -tt -T $2@$1 "echo '$pubkey' >> ~/.ssh/authorized_keys"
